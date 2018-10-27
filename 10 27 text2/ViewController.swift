fc3fda861a6d0eec17cbc00d99230097cb0de66d
Ciecs20171106520 <694522707@qq.com>
2018-10-27 08:57:49 +0800

    Initial Commit

---
+++ 10 27 text2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10 27 text2
+//
+//  Created by s20171106520 on 2018/10/27.
+//  Copyright © 2018 s20171106520. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
